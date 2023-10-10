a5b5d050026f87434eef4ee1b404aa6be539b1af
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-10-10 15:39:27 +0430

    update and find by ID added to server side

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,92 @@
-import express from "express"
-import mongoose from "mongoose"
-import { PORT, mongoDbURL } from "./config.js"
-import { Book } from "./models/bookModel.js"
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, mongoDbURL } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(234).send("Welcome to FullStack App!")
-})
+app.get("/", (req, res) => {
+  return res.status(234).send("Welcome to FullStack App!");
+});
 
-app.post('/books', async (req, res) => {
-    try {
-        if (
-            !req.body.title || 
-            !req.body.author ||
-            !req.body.publishedYear
-        ) {
-            return res.status(400).send({message: "Send all requires filed: author, title, and published year"})
-        }
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishedYear: req.body.publishedYear,
-        }
-        const book = await Book.create(newBook);
-        return res.status(201).send(book)
-    } catch (error) {
-        res.status(500).send({message: err.message})
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishedYear) {
+      return res.status(400).send({
+        message: "Send all requires filed: author, title, and published year",
+      });
     }
-})
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishedYear: req.body.publishedYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Find by ID
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// update a book
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishedYear) {
+      return res.status(400).send({
+        message: "send all required fields",
+      });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
-    .connect(mongoDbURL)
-    .then(() => {
-        console.log('App connected to Database');
-        app.listen(PORT, () => {
-            console.log(`server is listening to : ${PORT}`);
-        })
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+  .connect(mongoDbURL)
+  .then(() => {
+    console.log("App connected to Database");
+    app.listen(PORT, () => {
+      console.log(`server is listening to : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true
-        },
-        author: {
-            type: String,
-            required: true
-        },
-        publishedYear: {
-            type: Number,
-            required: true
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamp: true,
-    }
-)
+    author: {
+      type: String,
+      required: true,
+    },
+    publishedYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Book = mongoose.model('book', bookSchema)
+export const Book = mongoose.model("cat", bookSchema);
