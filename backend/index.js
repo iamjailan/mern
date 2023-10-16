4e34d191e1b62dfcf41c3a2f98f945572ac8a442
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-10-16 12:39:51 +0430

    axios installed and working with cors

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import { PORT, mongoDbURL } from "./config.js";
+import cors from "cors";
 import booksRoute from "./routes/booksRoute.js";
 
 app.use(express.json());
 app.use("/books", booksRoute);
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to FullStack App!");
