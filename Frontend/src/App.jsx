18678553f8b1ed08b338dd250accd8b2b8396fb9
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-10-18 09:04:10 +0430

    UI created for Book Store and user can see all  Books

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import CreateBook from "./pages/createBook";
+import DeleteBook from "./pages/deleteBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/editBook";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
